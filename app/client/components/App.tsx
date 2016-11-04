4c3fe101aae3d8d45d0071762cd8deb75328381c
csillag <csillag@harmacolindor.com>
2016-11-05 00:46:30 +0100

    Set app title and url

--- app/client/components/App.tsx
+++ app/client/components/App.tsx
@@ -9,9 +9,9 @@ import { GithubRibbon } from './GithubRibbon';
 export const App = () => (
         <Provider store={store}>
             <div>
-                <GithubRibbon url="csillag/template-10" />
+                <GithubRibbon url="csillag/person-lister" />
                 <div className="panel-heading">
-                    <h3 className="panel-title">App title</h3>
+                    <h3 className="panel-title">csillag's Person Lister</h3>
                 </div>
 
                 <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
