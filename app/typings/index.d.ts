8a1352cfd8cb7bc228c57ef9e66b9b9cc15f04aa
csillag <csillag@harmacolindor.com>
2016-11-06 03:05:21 +0100

    replace twbs:bootstrap with react-bootsrap

--- app/typings/index.d.ts
+++ app/typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/classnames/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/react-bootstrap/index.d.ts" />
 /// <reference path="globals/react-redux/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
